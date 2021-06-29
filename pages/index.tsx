7b17b28917479e18b5ce88dc70fde99f3ec8e7b9
baronTommy <16768208+baronTommy@users.noreply.github.com>
2021-06-29 22:50:32 +0900

    :sparkles:  study

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 import type { NextPage } from "next";
-import { Sample } from "~/presenter/Sample";
+import * as Tag from "~/presenter/Tag";
 
-const Page: NextPage = () => <Sample />;
+const Page: NextPage = () => (
+  <>
+    <Tag.Article />
+    <Tag.Nav />
+    <Tag.Aside />
+    <Tag.Section />
+    <Tag.Sample />
+    <Tag.Hgroup />
+    <Tag.Article2 />
+    <Tag.Paragraph />
+    <Tag.Line />
+    <Tag.Pre />
+    <Tag.Blockquote />
+    <Tag.List />
+    <Tag.ToolBar />
+    <Tag.DetailLIst />
+    <Tag.Figure />
+    <Tag.Overview />
+    <Tag.Link />
+    <Tag.Link2 />
+    <Tag.Text />
+    <Tag.Interactive />
+  </>
+);
 
 export default Page;
