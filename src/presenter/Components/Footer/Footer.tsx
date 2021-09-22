3cefda14701b021f63e570a40a4d0483ba13a857
baronTommy <16768208+baronTommy@users.noreply.github.com>
2021-09-22 10:35:38 +0900

    :lipstick:

--- src/presenter/Components/Footer/Footer.tsx
+++ src/presenter/Components/Footer/Footer.tsx
@@ -4,10 +4,29 @@ import * as S from "./Styled";
 export const Footer = () => (
   <S.Area>
     <S.LogoSection>
-      <a href="index.html">
+      <S.LogoLink href="index.html">
         <img src="/footer-logo.svg" alt="Tansaku!" />
-      </a>
+      </S.LogoLink>
     </S.LogoSection>
+    <S.NavSection>
+      <S.NavItems>
+        <S.NavItem>
+          <S.NavItemLink href="index.html">HOME</S.NavItemLink>
+        </S.NavItem>
+        <S.NavItem>
+          <S.NavItemLink href="post.html">最新の特集</S.NavItemLink>
+        </S.NavItem>
+        <S.NavItem>
+          <S.NavItemLink href="dummy">地域別に読む</S.NavItemLink>
+        </S.NavItem>
+        <S.NavItem>
+          <S.NavItemLink href="dummy">連載一覧</S.NavItemLink>
+        </S.NavItem>
+        <S.NavItem>
+          <S.NavItemLink href="dummy">インタビュー</S.NavItemLink>
+        </S.NavItem>
+      </S.NavItems>
+    </S.NavSection>
     <S.CopyRight>© Tansaku! All rights reserved.</S.CopyRight>
   </S.Area>
 );
--- src/presenter/Components/Footer/Styled.tsx
+++ src/presenter/Components/Footer/Styled.tsx
@@ -4,6 +4,7 @@ import { device } from "~/presenter/constraints";
 export const Area = styled.footer`
   color: #fff;
   background: #000;
+  padding: 60px 20px;
   @media ${device.pc} {
     max-width: 1040px;
     margin: 0 auto;
@@ -16,8 +17,45 @@ export const LogoSection = styled.div`
   text-align: center;
 `;
 
+export const LogoLink = styled.a`
+  display: inline-block;
+`;
+
 export const CopyRight = styled.p`
   font-size: 0.75rem;
   color: #d8d8d8;
   text-align: center;
+`;
+
+export const NavSection = styled.div`
+  margin: 0 0 30px 0;
+  padding: 0;
+`;
+
+export const NavItems = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-left: 0px;
+  @media ${device.pc} {
+    flex-direction: row;
+    justify-content: center;
+  }
+`;
+
+export const NavItem = styled.li`
+  margin: 0 0 2rem 0;
+  font-size: 0.875rem;
+  @media ${device.pc} {
+    margin: 0 1rem;
+  }
+`;
+
+export const NavItemLink = styled.a`
+  color: #d8d8d8;
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
 `;
