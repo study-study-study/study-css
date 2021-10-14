d3ade2d60d34edb39d14f5a2f2a7d0b2ff756019
baronTommy <1675038-Tommy-baron@users.noreply.gitlab.com>
2021-10-14 23:07:34 +0900

    :art:  abc

---
+++ .releaserc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  plugins: [
+    // '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        config: 'conventional-changelog-gitmoji-config',
+        releaseRules: [{ type: 'build', release: 'patch' }],
+      },
+    ],
+    // '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        config: 'conventional-changelog-gitmoji-config',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+  branches: ['main'],
+};
