620f6f757e50c6e0b27f00a81463bad096de160e
baronTommy <1675038-Tommy-baron@users.noreply.gitlab.com>
2021-10-14 23:44:17 +0900

    :art: x

--- .releaserc.js
+++ .releaserc.js
@@ -4,9 +4,10 @@ module.exports = {
       '@semantic-release/commit-analyzer',
       {
         releaseRules: [
-          { type: ':art:', release: 'patch' },
-          { type: ':art', release: 'patch' },
-          { type: 'art:', release: 'patch' },
+          //   { type: ':art:', release: 'patch' },
+          //   { type: ':art', release: 'patch' },
+          //   { type: 'art:', release: 'patch' },
+          { type: 'art', release: 'patch' },
         ],
       },
     ],
