58f1cc57aca3cd84415f41421f469636ab10acab
baronTommy <1675038-Tommy-baron@users.noreply.gitlab.com>
2021-10-15 09:12:14 +0900

    :art: x

--- .releaserc.js
+++ .releaserc.js
@@ -1,46 +1,16 @@
 module.exports = {
   plugins: [
-    // [
-    //   '@semantic-release/commit-analyzer',
-    //   {
-    //     parserOpts: {
-    //       noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
-    //     },
-    //     // headerPattern: /^(:.*?:) (.*)$/,
-    //     // releaseRules: [
-    //     //   { type: 'art', release: 'patch' },
-    //     //   { type: ':art', release: 'patch' },
-    //     //   { type: 'art:', release: 'patch' },
-    //     //   { type: ':art:', release: 'patch' },
-    //     //   { type: 'aaa', release: 'patch' },
-    //     //   { type: '\\:art', release: 'patch' },
-    //     //   { type: 'art\\:', release: 'patch' },
-    //     //   { type: '\\:art\\:', release: 'patch' },
-    //     // ],
-    //   },
-    // ],
     [
-      'semantic-release-gitmoji',
+      '@semantic-release/commit-analyzer',
       {
-        releaseRules: {
-          major: [':boom:'],
-          minor: [':sparkles:'],
-          patch: [':bug:', ':ambulance:', ':lock:', ':art:'],
-        },
-        // releaseNotes: {
-        //   template,
-        //   partials: { commitTemplate },
-        //   helpers: {
-        //     datetime: function (format = 'UTC:yyyy-mm-dd') {
-        //       return dateFormat(new Date(), format);
-        //     },
-        //   },
-        //   issueResolution: {
-        //     template: '{baseUrl}/{owner}/{repo}/issues/{ref}',
-        //     baseUrl: 'https://github.com',
-        //     source: 'github.com',
-        //   },
-        // },
+        config: 'conventional-changelog-gitmoji-config',
+        releaseRules: [{ type: 'art', release: 'patch' }],
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        config: 'conventional-changelog-gitmoji-config',
       },
     ],
     '@semantic-release/release-notes-generator',
