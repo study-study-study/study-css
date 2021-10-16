b2230c548e5e194bad5d401ad1065ef159fc7b9e
baronTommy <1675038-Tommy-baron@users.noreply.gitlab.com>
2021-10-16 12:06:39 +0900

    ":sparkles: foo

--- .releaserc.js
+++ .releaserc.js
@@ -1,7 +1,31 @@
 module.exports = {
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
+    // '@semantic-release/commit-analyzer',
+    // '@semantic-release/release-notes-generator',
+    [
+      'semantic-release-gitmoji',
+      {
+        releaseRules: {
+          major: [':boom:'],
+          minor: [':sparkles:'],
+          patch: [':bug:', ':ambulance:', ':lock:'],
+        },
+        releaseNotes: {
+          template,
+          partials: { commitTemplate },
+          helpers: {
+            datetime: function (format = 'UTC:yyyy-mm-dd') {
+              return dateFormat(new Date(), format);
+            },
+          },
+          issueResolution: {
+            template: '{baseUrl}/{owner}/{repo}/issues/{ref}',
+            baseUrl: 'https://github.com',
+            source: 'github.com',
+          },
+        },
+      },
+    ],
     '@semantic-release/github',
   ],
   branches: ['main'],
