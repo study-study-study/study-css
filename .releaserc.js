2e8b0dd2b9f95f209d52128f7a38aebde79f63d7
baronTommy <1675038-Tommy-baron@users.noreply.gitlab.com>
2021-10-16 11:01:08 +0900

    fix(pencil): stop graphite breaking when too much pressure applied

--- .releaserc.js
+++ .releaserc.js
@@ -1,15 +1,20 @@
 module.exports = {
+  // plugins: [
+  //   [
+  //     'semantic-release-gitmoji',
+  //     {
+  //       releaseRules: {
+  //         major: [':boom:'],
+  //         minor: [':sparkles:'],
+  //         patch: [':bug:', ':ambulance:', ':lock:'],
+  //       },
+  //     },
+  //   ],
+  //   '@semantic-release/github',
+  // ],
   plugins: [
-    [
-      'semantic-release-gitmoji',
-      {
-        releaseRules: {
-          major: [':boom:'],
-          minor: [':sparkles:'],
-          patch: [':bug:', ':ambulance:', ':lock:'],
-        },
-      },
-    ],
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
     '@semantic-release/github',
   ],
   branches: ['main'],
