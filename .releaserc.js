8fdd88d1fef5aad6d59fb94ee796dcc9de69a5eb
baronTommy <1675038-Tommy-baron@users.noreply.gitlab.com>
2021-10-16 12:08:07 +0900

    :boom: fix

--- .releaserc.js
+++ .releaserc.js
@@ -10,20 +10,6 @@ module.exports = {
           minor: [':sparkles:'],
           patch: [':bug:', ':ambulance:', ':lock:'],
         },
-        releaseNotes: {
-          template,
-          partials: { commitTemplate },
-          helpers: {
-            datetime: function (format = 'UTC:yyyy-mm-dd') {
-              return dateFormat(new Date(), format);
-            },
-          },
-          issueResolution: {
-            template: '{baseUrl}/{owner}/{repo}/issues/{ref}',
-            baseUrl: 'https://github.com',
-            source: 'github.com',
-          },
-        },
       },
     ],
     '@semantic-release/github',
