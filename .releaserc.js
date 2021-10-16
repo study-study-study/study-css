562c7215940a9d5582bb6b9acacb9947f7e2ddf8
baronTommy <1675038-Tommy-baron@users.noreply.gitlab.com>
2021-10-16 11:38:08 +0900

    fix(ccc): hoge
    fug
    
    x

--- .releaserc.js
+++ .releaserc.js
@@ -1,17 +1,4 @@
 module.exports = {
-  // plugins: [
-  //   [
-  //     'semantic-release-gitmoji',
-  //     {
-  //       releaseRules: {
-  //         major: [':boom:'],
-  //         minor: [':sparkles:'],
-  //         patch: [':bug:', ':ambulance:', ':lock:'],
-  //       },
-  //     },
-  //   ],
-  //   '@semantic-release/github',
-  // ],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
